f25787f1e5122a1c08c48b2b5b4b04f48e03c557
jspenjs17 <jspencer.js17@gmail.com>
2025-03-24 21:40:50 -0500

    3/24 Updates, Sumo working as far as I know

--- sumo/sumo.py
+++ sumo/sumo.py
@@ -13,7 +13,7 @@ backRightSpeed = 0
 
 def setSpeed(fl, bl, fr, br):
     global frontLeftSpeed, backLeftSpeed, frontRightSpeed, backRightSpeed
-    maxSpeedConstant = 0.25
+    maxSpeedConstant = .60
     frontLeftSpeed = fl *maxSpeedConstant
     backLeftSpeed = bl *maxSpeedConstant
     frontRightSpeed = fr *maxSpeedConstant
@@ -77,7 +77,7 @@ class MecanumChassis:
         return msg
 
 class Camera:
-    def __init__(self, x, y, size):
+    def __init__(self):
         #fix to actually get values
         self.colourXvalue =0
         self.colourYvalue =0
@@ -89,45 +89,55 @@ class Camera:
         self.colourSize   = size
 
 class Lidar:
-    def __init__(self, front, back, right, left):
+    def __init__(self):
         #fix to actually get values
         # maybe average front/left/back/right?
-        self.frontDistance =0
-        self.backDistance =0
-        self.rightDistance =0
-        self.leftDistance =0
+        self.front = -1
+        self.left = -1
+        self.back = -1
+        self.right = -1
+        self.frontLeft = -1
+        self.frontRight = -1
+        self.backLeft = -1
+        self.backRight = -1
 
-        self.nearestDistance =0 #calculable
+        self.nearestDistance = 0 # calculable
+
+def divNoZero(num, div):
+    if div == 0:
+        return 0
+    else:
+        return num/div    
 
 class SumoNode(Node):
-    def __init__(self, name="ListenerNode"):
+    def __init__(self, name="ListenerNode", die = False):
         super().__init__(name, allow_undeclared_parameters=True, automatically_declare_parameters_from_overrides=True)
 
-        # subscription to camera output
-        self.create_subscription(ColorsInfo, '/color_detect/color_info', self.update_data, 10)
-        self.lidar_sub = self.create_subscription(LaserScan, '/scan_raw', self.update_data, 10)
-        self.get_logger().info(f"Setup Complete")
-
         # initialize the camera and lidar data
-        self.camera = Camera(None, None, None)
-        self.lidar = Lidar(None, None, None, None)
+        self.camera = Camera()
+        self.lidar = Lidar()
         self.detected_color = False
 
         # init motor publisher
         self.motor_pub = self.create_publisher(MotorsState, 'ros_robot_controller/set_motor', 1)
         # init mecanum chassis controller object
-        self.mecanum = MecanumChassis()    
+        self.mecanum = MecanumChassis()
 
-        #pick a random number for whether the robot should circle left or right.
-        #main goal is to be unpredicatble
-        if random.random() < 0.5:
-            setSpeed(-1, -1, -1, 1)
-        else:
-            setSpeed(-1, 1, -1, -1)
+        # subscription to camera output
+        self.create_subscription(ColorsInfo, '/color_detect/color_info', self.update_data, 10)
+        self.lidar_sub = self.create_subscription(LaserScan, '/scan_raw', self.update_data, 10)
 
-        # intial movment to try and dodge out of the opponenets way
-        self.set_velocity()
-        time.sleep(0.5)
+        if die:
+            print("Dying!")
+            setSpeed(0, 0, 0, 0)
+            self.set_velocity()
+            return
+        
+        self.leftOrRight = random.random() < .5
+        self.count = 0
+
+        
+        
 
     def set_velocity(self):
         # print(f"Setting velocity to: ({frontLeftSpeed}, {backLeftSpeed}, {frontRightSpeed}, {backRightSpeed})")
@@ -154,20 +164,19 @@ class SumoNode(Node):
             # ranges: 0 to 2pi
             # 0 is dead ahead, so we want -45 degrees to 45 degrees to represent forward
             # increments COUNTERCLOCKWISE from 0
-            # so ranges are:
-                # 45-135 : left
-                # 135-225: back
-                # 225-315: right
-                # 315-45 : front
             # the goal is to add together all values associated with these angles and average them
             # then load that average into their respective "bin" in the Lidar class
             
             sums = {
-                # side : sum, count    <-- for averaging
-                "left" : [0, 0],
-                "back" : [0, 0],
-                "right": [0, 0],
-                "front": [0, 0],
+                # side : index, sum, count    <-- for averaging
+                "front"     : [0, 0],
+                "left"      : [0, 0],
+                "back"      : [0, 0],
+                "right"     : [0, 0],
+                "frontLeft" : [0, 0],
+                "frontRight": [0, 0],
+                "backLeft"  : [0, 0],
+                "backRight" : [0, 0],
             }
 
             # start with min angle
@@ -176,20 +185,32 @@ class SumoNode(Node):
             side_str = "front"
             # loop until we exceed max, at which point we're done
             while angle < msg.angle_max and index < len(msg.ranges):
-                # angle between 45-135, left side of robot
-                if angle >= math.radians(45) and angle < math.radians(135):
-                    side_str = "left"
+                # find where we are in the circle based
+                if angle >= math.radians(0) and angle < math.radians(22.5):
+                    side_str = "front"
+                
+                elif angle >= math.radians(45 - 22.5) and angle < math.radians(45 + 22.5):
+                    side_str = "frontLeft"
 
-                # angle between 135-225, back of robot
-                elif angle >= math.radians(135) and angle < math.radians(225):
+                elif angle >= math.radians(90 - 22.5) and angle < math.radians(90 + 22.5):
+                    side_str = "left"
+                
+                elif angle >= math.radians(135 - 22.5) and angle < math.radians(135 + 22.5):
+                    side_str = "backLeft"
+
+                elif angle >= math.radians(180 - 22.5) and angle < math.radians(180 + 22.5):
                     side_str = "back"
 
-                # angle between 225-315, right side of robot
-                elif angle >= math.radians(225) and angle < math.radians(315):
+                elif angle >= math.radians(225 - 22.5) and angle < math.radians(225 + 22.5):
+                    side_str = "backRight"
+
+                elif angle >= math.radians(270 - 22.5) and angle < math.radians(270 + 22.5):
                     side_str = "right"
-                
-                # angle between 315-45 -- since it wraps around, we need to check both ends
-                elif angle >= math.radians(315) and angle < msg.angle_max or angle >= msg.angle_min and angle < math.radians(45):
+
+                elif angle >= math.radians(315 - 22.5) and angle < math.radians(315 + 22.5):
+                    side_str = "frontRight"
+
+                else: # between 315 + 22.5 and 360
                     side_str = "front"
 
                 # make sure we're not doing math with nans
@@ -204,22 +225,44 @@ class SumoNode(Node):
                 index += 1
 
             # load in the data to our lidar data structure
-            self.lidar.leftDistance  = sums["left"][0] /sums["left"][1]
-            self.lidar.backDistance  = sums["back"][0] /sums["back"][1]
-            self.lidar.rightDistance = sums["right"][0]/sums["right"][1]
-            self.lidar.frontDistance = sums["front"][0]/sums["front"][1]
+            self.lidar.front =      divNoZero(sums["front"][0], sums["front"][1])
+            self.lidar.back =       divNoZero(sums["back"][0], sums["back"][1])
+            self.lidar.left =       divNoZero(sums["left"][0], sums["left"][1])
+            self.lidar.right =      divNoZero(sums["right"][0], sums["right"][1])
+            self.lidar.frontLeft =  divNoZero(sums["frontLeft"][0], sums["frontLeft"][1])
+            self.lidar.frontRight = divNoZero(sums["frontRight"][0], sums["frontRight"][1])
+            self.lidar.backLeft =   divNoZero(sums["backLeft"][0], sums["backLeft"][1])
+            self.lidar.backRight =  divNoZero(sums["backRight"][0], sums["backRight"][1])
 
-            # # neat printout to make sure it's working, the .25 is arbitrary and VERY close
-            # if self.lidar.leftDistance < .25:
-            #     print("Close left")
-            # elif self.lidar.backDistance < .25:
-            #     print("Close back")
-            # elif self.lidar.rightDistance < .25:
-            #     print("Close right")
-            # elif self.lidar.frontDistance < .25:
+            # if self.lidar.front < .25:
             #     print("Close front")
+            # elif self.lidar.back < .25:
+            #     print("Close back")
+            # elif self.lidar.left < .25:
+            #     print("Close left")
+            # elif self.lidar.right < .25:
+            #     print("Close right")
+            # elif self.lidar.frontLeft < .25:
+            #     print("Close frontLeft")
+            # elif self.lidar.frontRight < .25:
+            #     print("Close frontRight")
+            # elif self.lidar.backLeft < .25:
+            #     print("Close backLeft")
+            # elif self.lidar.backRight < .25:
+            #     print("Close backRight")
             # else:
-            #     print("Nothing close")
+            #     print("Not close")
+
+            self.lidar.nearestDistance = min(
+                # self.lidar.front,
+                self.lidar.back,
+                self.lidar.left,
+                self.lidar.right,
+                self.lidar.frontLeft,
+                self.lidar.frontRight,
+                self.lidar.backLeft,
+                self.lidar.backRight,
+            )
 
         # now onto the main movement algorithm with our updated information
         self.move()
@@ -229,60 +272,91 @@ class SumoNode(Node):
 
     def move(self):
         # make sure lidar and camera data exists
-        if self.lidar.frontDistance == None:
+        if self.lidar.front == -1:
             print("No lidar data")
-            return            print(self.lidar.backDistance)
+            # return
 
         elif self.camera.colourXvalue == None:
             print("No camera data")
-            return
+            # return
 
         #set variables
         middleOfCamera = 320 #this shouldn't be changed
-        lowerBoundForRamming = 110 #increase to make it ram when it is better lined up, decrease it if it needs to ram sooner
-        upperBoundForRamming = middleOfCamera + (middleOfCamera-lowerBoundForRamming) #shouldn't need to be changed
-        touchingThreshold = 25 #thershold for how big the orange is to know to ram. Increase to make it wait closer to full send. Decrease to make it full send sooner
-        dangerThreshold = .5 #thershold for how close to the wall before the robot is before it moves away. Increase to make it stay further from the wall
-        kP = 0.01 #do not increase past 1/(middleOfCamera-lowerBoundForRamming), decrese to make the robot line up with the other slower
+        #lowerBoundForRamming = 0 #increase to make it ram when it is better lined up, decrease it if it needs to ram sooner
+        #upperBoundForRamming = middleOfCamera + (middleOfCamera-lowerBoundForRamming) #shouldn't need to be changed
+        touchingThreshold = 50 #thershold for how big the orange is to know to ram. Increase to make it wait closer to full send. Decrease to make it full send sooner
+        dangerThreshold = .35 #thershold for how close to the wall before the robot is before it moves away. Increase to make it stay further from the wall
+        kP = 1/(middleOfCamera) #do not increase past 1/(middleOfCamera-lowerBoundForRamming), decrease to make the robot line up with the other slower
+        
 
         #1st priority is to ram into an opponenet if we are lined up perfectly
-        if(self.camera.colourXvalue > lowerBoundForRamming and self.camera.colourXvalue < upperBoundForRamming):
-            #ram straight into the other robot if you are really close
-            if(self.camera.colourSize > touchingThreshold):
-                print("Ramming!!!")
+        if (self.count < 12):
+            print(self.count)
+            #pick a random number for whether the robot should circle left or right.
+            #main goal is to be unpredicatble
+            if self.leftOrRight:
+                setSpeed(1, -1, -1, 1)
+            else:
+                setSpeed(-1, 1, 1, -1)
+
+            self.count += 1
+
+        elif(self.count > 30):
+            setSpeed(1, 1, 1, 1)
+            self.count -= 1
+
+        #ram straight into the other robot if you are really close
+        elif(self.camera.colourSize > touchingThreshold):
+            print("Ramming!!!")
+            setSpeed(1, 1, 1, 1)
+            self.count = 40
+
+        elif(self.camera.colourXvalue > 0 and self.camera.colourXvalue < middleOfCamera * 2):
+            print("Strafing towards opponents")
+            #using strafing capabilites of mecanum drive to better line up with the target while ramming
+            lateralError = self.camera.colourXvalue-middleOfCamera
+            print(f"Lateral Error: {lateralError}")
+            #execute this if the target is left of the centre 
+            if(lateralError < 0):
+                #to strafe left slightly, give the front left and back right wheels less power
+                setSpeed(1- abs(lateralError) * kP, 1, 1, 1- abs(lateralError) * kP)
+            #execute this if the target is right of the centre 
+            else:
+                #to strafe right slightly, give the back left and front right wheels less power
+                setSpeed(1, 1- abs(lateralError) * kP, 1- abs(lateralError) * kP, 1)
+
+        # check for walls
+        elif self.lidar.nearestDistance < dangerThreshold:
+            if self.lidar.frontLeft == self.lidar.nearestDistance:
+                print("frontLeft Danger Threshold hit")
+                setSpeed(0, -1, -1, 0)
+
+            elif self.lidar.left == self.lidar.nearestDistance:
+                print("left Danger Threshold hit")
+                setSpeed(1, -1, -1, 1)
+
+            elif self.lidar.backLeft == self.lidar.nearestDistance:
+                print("backLeft Danger Threshold hit")
+                setSpeed(1, 0, 0, 1)
+
+            elif self.lidar.back == self.lidar.nearestDistance:
+                print("back Danger Threshold hit")
                 setSpeed(1, 1, 1, 1)
 
+            elif self.lidar.backRight == self.lidar.nearestDistance:
+                print("backRight Danger Threshold hit")
+                setSpeed(0, 1, 1, 0)
+
+            elif self.lidar.right == self.lidar.nearestDistance:
+                print("Back Danger Threshold hit")
+                setSpeed(-1, 1, 1, -1)
+            
+            elif self.lidar.frontRight == self.lidar.nearestDistance:
+                print("Back Danger Threshold hit")
+                setSpeed(-1, 0, 0, -1)
+
             else:
-                #using strafing capabilites of mecanum drive to better line up with the target while ramming
-                lateralError = self.camera.colourXvalue-middleOfCamera
-
-                print("Strafing towards opponents")
-                #execute this if the target is left of the centre 
-                if(lateralError < 0):
-                    #to strafe left slightly, give the front left and back right wheels less power
-                    setSpeed(1- abs(lateralError) * kP, 1, 1, 1- abs(lateralError) * kP)
-                #execute this if the target is right of the centre 
-                else:
-                    #to strafe right slightly, give the back left and front right wheels less power
-                    setSpeed(1, 1- abs(lateralError) * kP, 1- abs(lateralError) * kP, 1)
-
-        #2nd priorirt check how close to the wall the robot is
-        elif(self.lidar.backDistance < dangerThreshold):
-            print("Back Danger Threshold hit")
-            #drive away from the wall
-            setSpeed(1, 1, 1, 1)
-
-        #check for objects to the right
-        elif(self.lidar.rightDistance < dangerThreshold):
-            print("Right Danger Threshold hit")
-            #drive away from the wall
-            setSpeed(-1, 1, 1, -1)
-
-        #check for objects to the left
-        elif(self.lidar.leftDistance < dangerThreshold):
-            print("Left Danger Threshold hit")
-            #drive away from the wall
-            setSpeed(1, -1, -1, 1)
+                print("front/frontLeft/frontRight Danger Threshold hit, ignoring")
 
         #3rd priorty is prepare to ram the other team
         elif(self.camera.colourSize != -1):
@@ -300,12 +374,14 @@ class SumoNode(Node):
         #turn to try and find the other robot
         else:
             print("No robot found, turning to find")
-            setSpeed(1, 1, -1, -1)
+            if self.leftOrRight:
+                setSpeed(-.5, -.5, .5, .5)
+            else:
+                setSpeed(.5, .5, -.5, -.5)
+
             
 def sigint_handler(sig, frame):
-    sumo = SumoNode("sumo_node_die")
-    setSpeed(0, 0, 0, 0)
-    sumo.set_velocity()
+    sumo = SumoNode("sumo_node_die", die = True)
     sys.exit(0)
 
 def main():
